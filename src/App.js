cb838a661903a05218b78d7f3f879a1be1bd47a5
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-04-07 03:49:46 +0530

    fix: added manifest.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { UserOnboard } from './pages/userOnboard';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <UserOnboard />
     </div>
   );
 }
-
-export default App;
