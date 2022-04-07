381a0e389910306f12ed61fb2b664f87d8c4802a
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-04-07 11:53:13 +0530

    added:meta tags

--- src/components/quote/quote.jsx
+++ src/components/quote/quote.jsx
@@ -1,7 +1,6 @@
 import animeQuotesDB from "../../quotes.json";
 export const Quote = () => {   
         const animeQuoteGenerator = (quotesArr) => quotesArr[Math.floor(Math.random()*quotesArr.length)]
-
         const animeQuote = animeQuoteGenerator(animeQuotesDB.animeQuotes);
         const currentQuote = animeQuote.quote;
         const currentAuthor = animeQuote.author;
