0feb81f2fd814699366f85cd93a096e082885743
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-04-07 22:52:16 +0530

    fix: main focus is not working upon enter key

--- src/components/mainFocus/mainfocus.jsx
+++ src/components/mainFocus/mainfocus.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 
 export const MainFocus=()=> {
     const [edit, setEdit] = useState(false)
+    const [focusMessage, setFocusMessage] = useState(null)
     const focusEditHandler = () => setEdit(true)
-    const focusEntered = localStorage.getItem('focus');
     
     const mainFocusHandler=(e)=> {
         if(e.key === 'Enter') {
-            localStorage.setItem('focus', e.target.value,);
+            localStorage.setItem('focus', e.target.value);
+            setFocusMessage(e.target.value)
             setEdit(false)
         }
     }
@@ -23,14 +24,14 @@ export const MainFocus=()=> {
     const ShowMainFocus=()=> {
         return (
             <div>
-                <h1>Your main focus for today is {focusEntered} </h1> 
+                <h1>Your main focus for today is {focusMessage} </h1> 
                 <button className="edit-btn" onClick={focusEditHandler}>Edit</button>
             </div>
         )
     }
     return (
     <div >
-        { focusEntered === null || edit ? <EnterMainFocus/> : <ShowMainFocus />}
+        { focusMessage === null || edit ? <EnterMainFocus/> : <ShowMainFocus />}
     </div>
     )
 }
